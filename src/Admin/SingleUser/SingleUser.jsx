648c746d2f671c28a91dbcfa065e3965614d87c6
neo1415 <adedaniel502@gmail.com>
2023-05-09 14:33:54 +0100

    individual

--- src/Admin/SingleUser/SingleUser.jsx
+++ src/Admin/SingleUser/SingleUser.jsx
@@ -36,8 +36,21 @@ const SingleUser = () => {
 
     const downloadPDF = () => {
         const doc = new jsPDF('p', 'pt', 'a4');
-        const tableColumn = ['Field', 'Value'];
-        const tableRows = [ ['Company Name', data.companyName],
+      
+        // Add header
+        doc.setFontSize(24);
+        doc.setTextColor(128, 0, 32);
+        doc.text('NEM Insurance Corporate KYC Form', 50, 70);
+      
+        // Add section 1 - Company Information
+        doc.setFontSize(18);
+        doc.setTextColor(0, 0, 0);
+
+        doc.setFontSize(16);
+        doc.text('Company Details', 60, 120);
+      
+        const companyTableColumn = ['Company Information', ''];
+        const companyTableRows = [    ['Company Name', data.companyName],
           ['Registered Company Address', data.registeredCompanyAddress],
           ['Contact Telephone Number', data.contactTelephoneNumber],
           ['Email Address', data.emailAddress],
@@ -45,12 +58,13 @@ const SingleUser = () => {
           ['Contact Person', data.contactPerson],
           ['Tax Identification Number', data.taxIdentificationNumber],
           ['VAT Registration Number', data.VATRegistrationNumber],
-          ['Date of Incorporation Registration', data.dateofIncorporationRegistration],
+          ['Date of Incorporation Registration', data.dateOfIncorporationRegistration],
           ['Incorporation State', data.incorporationState],
           ['Company Type', data.companyType],
         ];
-        doc.autoTable(tableColumn, tableRows, {
-          startY: 60,
+      
+        const companyTableProps = {
+          startY: 140,
           styles: {
             halign: 'left',
             valign: 'middle',
@@ -70,15 +84,176 @@ const SingleUser = () => {
               textColor: [0, 0, 0],
             },
           },
-          headerStyles: {
-            fillColor: [0, 102, 153],
-            textColor: [255, 255, 255],
-            fontStyle: 'bold',
+        };
+      
+        doc.autoTable(companyTableColumn, companyTableRows, companyTableProps);
+      
+        // Add sub-section - Directors Information
+        doc.setFontSize(18);
+        doc.setTextColor(0, 0, 0);
+        doc.text('Directors Information', 40, doc.lastAutoTable.finalY + 60);
+
+      
+        const directorsTableColumn = ['Director One', ''];
+        const directorsTableRows = [    
+          ['Date of Birth', data.dob],
+          ['First Name', data.firstName],
+          ['Last Name', data.lastName],
+          ['Residential Address', data.residentialAddress],
+          ['Issuing Body', data.issuingBody],
+        ];
+      
+        const directorsTableProps = {
+          startY: doc.lastAutoTable.finalY + 80,
+          styles: {
+            halign: 'middle',
+            valign: 'middle',
+            fontSize: 12,
+            cellPadding: 8,
+            overflow: 'linebreak',
+            lineWidth: 0.1,
           },
-          margin: { top: 50, left: 40, right: 40, bottom: 40 },
-        });
-        doc.save('user.pdf');
-      };
+          columnStyles: {
+            0: {
+              fillColor: [255, 255, 255],
+              textColor: [0, 0, 0],
+              fontStyle: 'bold',
+            },
+            1: {
+              fillColor: [255, 255, 255],
+              textColor: [0, 0, 0],
+            },
+          },
+        };
+      
+        doc.autoTable(directorsTableColumn, directorsTableRows, directorsTableProps);
+      
+        // Add sub-section - Second Director's Information
+      
+const secondDirectorsTableColumn = ['Director Two', ''];
+const secondDirectorsTableRows = [
+    ['Date of Birth', data.dob2],
+    ['First Name', data.firstName2],
+    ['Last Name', data.lastName2],
+    ['Residential Address', data.residentialAddress2],
+    ['Issuing Body', data.issuingBody2],
+];
+
+const secondDirectorsTableProps = {
+startY: doc.lastAutoTable.finalY + 50,
+startX: 300,
+styles: {
+halign: 'middle',
+valign: 'middle',
+fontSize: 12,
+cellPadding: 8,
+overflow: 'linebreak',
+lineWidth: 0.1,
+},
+columnStyles: {
+0: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+fontStyle: 'bold',
+},
+1: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+},
+},
+};
+
+doc.autoTable(secondDirectorsTableColumn, secondDirectorsTableRows, secondDirectorsTableProps);
+
+// Add section 2 - Beneficial Owners Information
+doc.setFontSize(18);
+doc.setTextColor(0, 0, 0);
+doc.text('Account Details', 40, doc.lastAutoTable.finalY + 60);
+
+const beneficialOwnersTableColumn = ['Naira Account', ''];
+const beneficialOwnersTableRows = [
+['Account Number', data.accountNumber],
+['Bank Name', data.bankName],
+['Bank Branch', data.bankBranch],
+['Acount Opening Date', data.accountOpeningDate],
+];
+
+const beneficialOwnersTableProps = {
+startY: doc.lastAutoTable.finalY + 80,
+styles: {
+halign: 'middle',
+valign: 'middle',
+fontSize: 12,
+cellPadding: 8,
+overflow: 'linebreak',
+lineWidth: 0.1,
+},
+columnStyles: {
+0: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+fontStyle: 'bold',
+},
+1: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+},
+},
+};
+
+doc.autoTable(beneficialOwnersTableColumn, beneficialOwnersTableRows, beneficialOwnersTableProps);
+
+// Add sub-section - Beneficial Owner 2 Information
+
+const secondBeneficialOwnersTableColumn = ['Dollar Account', ''];
+const secondBeneficialOwnersTableRows = [
+    ['Account Number', data.accountNumber2],
+    ['Bank Name', data.bankName2],
+    ['Bank Branch', data.bankBranch2],
+    ['Acount Opening Date', data.accountOpeningDate2],
+];
+
+const secondBeneficialOwnersTableProps = {
+startY: doc.lastAutoTable.finalY + 60,
+styles: {
+halign: 'middle',
+valign: 'middle',
+fontSize: 12,
+cellPadding: 8,
+overflow: 'linebreak',
+lineWidth: 0.1,
+},
+columnStyles: {
+0: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+fontStyle: 'bold',
+},
+1: {
+fillColor: [255, 255, 255],
+textColor: [0, 0, 0],
+},
+},
+};
+
+doc.autoTable(secondBeneficialOwnersTableColumn, secondBeneficialOwnersTableRows, secondBeneficialOwnersTableProps);
+
+// Add section 3 - Declaration and Signature
+// doc.setFontSize(18);
+// doc.setTextColor(0, 0, 0);
+// doc.text('Section 3 - Declaration and Signature', 40, doc.lastAutoTable.finalY + 60);
+
+// doc.setFontSize(12);
+// doc.text('I hereby declare that the information provided in this form is true and correct to the best of my knowledge and belief. I understand that any false or misleading statement may result in the rejection of this application.', 60, doc.lastAutoTable.finalY + 100);
+
+// doc.setFontSize(16);
+// doc.text('Signature:', 60, doc.lastAutoTable.finalY + 200);
+// doc.line(140, doc.lastAutoTable.finalY + 200, 350, doc.lastAutoTable.finalY + 200);
+
+// Save the PDF
+doc.save('KYC Form.pdf');
+    }
+      
 
   return (
     <div className='singles'>
@@ -123,7 +298,7 @@ const SingleUser = () => {
             </li>
             <li className='form-list'>
                     <p>Date of Incorporation Registration</p>
-                    <p className='info'>{data.dateofIncorporationRegistration}</p>
+                    <p className='info'>{data.dateOfIncorporationRegistration}</p>
             </li>
             <li className='form-list'>
                 <p>Incorporation State</p>
@@ -161,27 +336,27 @@ const SingleUser = () => {
                     <p className='info'>{data.issuingBody}</p>
             </li>
             </ul>
-            <ul className='form-ul'>
+            <ul>
             <h1>Directors Profile 2</h1>
             <li className='form-list'>
                     <p>Date Of Birth</p>
-                    <p className='info'>{data?.dob2}</p>
+                    <p className='info'>{data.dob2}</p>
             </li>
             <li className='form-list'>
                     <p>First Name</p>
-                    <p className='info'>{data?.firstName2}</p>
+                    <p className='info'>{data.firstName2}</p>
             </li>
             <li className='form-list'>
                     <p>Last Name</p>
-                    <p className='info'>{data?.lastName2}</p>
+                    <p className='info'>{data.lastName2}</p>
             </li>
             <li className='form-list'>
                     <p>Residential Address</p>
-                    <p className='info'>{data?.residentialAddress2}</p>
+                    <p className='info'>{data.residentialAddress2}</p>
             </li>
             <li className='form-list'>
                     <p>Issuing Body</p>
-                    <p className='info'>{data?.issuingBody2}</p>
+                    <p className='info'>{data.issuingBody2}</p>
             </li>
             </ul>
             </div>
@@ -230,7 +405,7 @@ const SingleUser = () => {
         </div>
     </div>
     <div className='file-download'>
-    <button onClick={downloadPDF}>Download Form <HiDownload style={style} /> </button>
+    <button className='form-button' onClick={downloadPDF}>Download Form <HiDownload style={style} /> </button>
     </div>
     </div>
     
