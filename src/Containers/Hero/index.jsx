9deacce4c21580395ce9b2c3b36f07fee1338c3d
neo1415 <adedaniel502@gmail.com>
2023-05-12 13:12:31 +0100

    hero

--- src/Containers/Hero/index.jsx
+++ src/Containers/Hero/index.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="hero-flex">
 <div className="content">
-  <h2 class="heading">Trusted Identity Verification for Today's Digital World</h2>
+  <h2 class="heading">NEM INSURANCE KYC/CDD Information Collection Center</h2>
   <p class="sub">Streamlined KYC and CDD Solutions for Secure Online Transactions.</p>
 <a className="smooth-scroll" href='#section'>
   <button class="main__button">Get started</button>
