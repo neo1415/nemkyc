3a75cfd96958b89201d823db27b2ae9bffe64ba9
neo1415 <adedaniel502@gmail.com>
2023-05-12 12:52:18 +0100

    desc

--- src/Admin/Table/index.jsx
+++ src/Admin/Table/index.jsx
@@ -25,7 +25,7 @@ const List = () => {
   
   useEffect(()=> {
     const dataRef = collection(db, 'users')
-    const q = query(dataRef, orderBy('createdAt', 'asc'));
+    const q = query(dataRef, orderBy('createdAt', 'desc'));
     onSnapshot(q,(snapshot) => {
       const data = snapshot.docs.map((doc) => ({
         id:doc.id,
