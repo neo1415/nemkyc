90aa8ce1ceeaa11a55f82a98c2834dcbc4bc1be7
neo1415 <adedaniel502@gmail.com>
2023-07-24 21:49:05 +0100

    responsive modals

--- src/Containers/Section/section.jsx
+++ src/Containers/Section/section.jsx
@@ -2,26 +2,17 @@ import React from 'react'
 import { images } from '../../Constants';
 import './Section.scss'
 import { Link } from 'react-router-dom';
+import CorporateWrapper from '../Modals/sectionModal';
+import IndividualWrapper from '../Modals/IndividualSection';
 
 const Section = () => {
   return (
 <section id='section'>
-<Link to='/corporate-kyc'>
-  <div class="card">
-    <img src={images.bg2} alt="Image 1" />
-    <div class="overlay">
-      <h2>CORPORATE<br/>FORM</h2>
-    </div>
-  </div>
-  </Link>
-  <Link to='/individual-kyc'>
-  <div class="card">
-    <img src={images.bg1} alt="Image 2" />
-    <div class="overlay">
-      <h2>INDIVIDUAL <br /> FORM</h2>
-    </div>
-  </div>
-  </Link>
+
+    <CorporateWrapper />
+
+    <IndividualWrapper />
+
 </section>
 
   )
