12c063d95b8c5b73635e6f66f8855194fbb617fa
neo1415 <adedaniel502@gmail.com>
2023-09-28 10:55:05 +0100

    user roles and password reset

--- src/Admin/Authentication/ResetPassword.js
+++ src/Admin/Authentication/ResetPassword.js
@@ -1,49 +1,43 @@
-// import React,{useState} from 'react'
-// import { Link, useNavigate } from 'react-router-dom'
-// import { UserAuth } from '../../Context/AuthContext'
-// import './form.scss'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../../Context/AuthContext';
+import './form.scss';
 
-// const ResetPassword = () => {
+const ResetPassword = () => {
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
 
-//     const [error, setError] = useState(false)
-//     const [email, setEmail] = useState('')
+  const { resetPassword } = UserAuth();
 
-// const {resetPassword} = UserAuth()
+  const navigate = useNavigate();
 
-// const navigate=useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(''); // Clear any previous errors
+    try {
+      await resetPassword(email);
+      alert("Password reset link sent!");
+      navigate('/signin');
+    } catch (e) {
+      setError(e.message);
+    }
+  };
 
+  return (
+    <div className='login'>
+      <div className='loginTitle'>
+        <h2>Enter Your Email to Reset Your Password</h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='Inputs'>
+          <label>E-mail</label>
+          <input type='email' onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <button>Send Email</button>
+        {error && <p className="error-message">{error}</p>} {/* Display error message if an error occurs */}
+      </form>
+    </div>
+  );
+};
 
-
-// const handleSubmit = async (e) => {
-//     e.preventDefault()
-//     setError('')
-//     try {
-//       await resetPassword(email);
-//       alert("Password reset link sent!");
-//       navigate('/signin')
-//     } catch(e){
-//         setError(e.message)
-//         // console.log(e.message)
-//     }
-//   };
-
-
-
-//   return (
-//     <div className='login'>
-//     <div className='loginTitle'>
-//         <h2>Enter Your Email to reset your password</h2>
-//     </div>
-//     <form onSubmit={handleSubmit}> 
-//     <div className='Inputs'>        
-//              <label>E-mail</label>
-//             <input type='email'  onChange={(e)=> setEmail(e.target.value)} />
-//         </div>
-//         <button>Send Email</button>
-
-//     </form>
-// </div>
-//   )
-// }
-
-// export default ResetPassword
+export default ResetPassword;
