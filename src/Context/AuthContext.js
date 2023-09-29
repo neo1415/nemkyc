19be723fb631e4526851569c54641952838c19c0
neo1415 <adedaniel502@gmail.com>
2023-09-29 01:38:34 +0100

    local storage

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -33,16 +33,34 @@ const logout= ()=>{
 }
 
 useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-    // console.log(currentUser)
-    setUser(currentUser)
-  })
-  return () => {
-    unsubscribe()
-  }
-  
-}, []);
+  // Get user data from local storage on component mount
+  const storedUser = JSON.parse(localStorage.getItem('user'));
 
+  // Set the initial user state from local storage
+  if (storedUser) {
+    setUser(storedUser);
+  }
+
+  // Set up an observer to track user authentication state changes
+  const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+    if (authUser) {
+      // User is signed in
+      setUser(authUser);
+
+      // Store user data in local storage on login
+      localStorage.setItem('user', JSON.stringify(authUser));
+    } else {
+      // User is signed out
+      setUser(null);
+
+      // Remove user data from local storage on logout
+      localStorage.removeItem('user');
+    }
+  });
+
+  // Clean up the observer on unmount
+  return () => unsubscribe();
+}, []);
   return(
     <UserContext.Provider value={{createUser, user, logout, signIn, resetPassword}}>
       {children}
