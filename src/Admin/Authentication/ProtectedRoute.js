4f40a0cdbca7c9c6e7fde1a601c2b7142d35dc45
neo1415 <adedaniel502@gmail.com>
2023-10-20 13:36:22 +0100

    cache

--- src/Admin/Authentication/ProtectedRoute.js
+++ src/Admin/Authentication/ProtectedRoute.js
@@ -7,30 +7,30 @@ import PageLoad from '../../Components/PageLoad';
 
 const ProtectedRoute = ({ children, adminOnly, moderatorOnly }) => {
   const { user } = UserAuth();
-  const [isLoading, setIsLoading] = useState(true); // Loading state
-  const [userRole, setUserRole] = useState(localStorage.getItem('userRole')); // Initialize with cached role
-  
+  const [isLoading, setIsLoading] = useState(!localStorage.getItem('userRole')); // Display loading only if user role is not in cache
+  const [userRole, setUserRole] = useState(localStorage.getItem('userRole'));
+
   useEffect(() => {
     const fetchUserRole = async () => {
       if (user) {
         try {
-          // Check if the user role is already cached in localStorage
+          const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
+
           const cachedRole = localStorage.getItem('userRole');
           if (cachedRole) {
             setUserRole(cachedRole);
           }
 
-          const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
           const response = await axios.post(`${serverURL}/check-user-role/${user.uid}`);
           const role = response.data.role;
 
-          // Update the user role and cache it
           setUserRole(role);
           localStorage.setItem('userRole', role);
+
+          // Update isLoading when the user role is available
+          setIsLoading(false);
         } catch (error) {
           console.error('Error checking user role:', error);
-        } finally {
-          setIsLoading(false);
         }
       }
     };
@@ -38,37 +38,30 @@ const ProtectedRoute = ({ children, adminOnly, moderatorOnly }) => {
     fetchUserRole();
   }, [user]);
 
-
   console.log('User Role:', userRole);
 
-  // Check if the loading state is true, render loading component
   if (isLoading) {
     return (
-<PageLoad />
-    ); // Replace with your loading component
+      <PageLoad />
+    );
   }
 
-  // Check if the user is logged in
   if (!user) {
     return <Navigate to="/signin" />;
   }
 
-  // Check if the user role is not 'loading' (role fetched) and proceed
   if (userRole !== 'loading') {
-    // Check if the route is admin-only and the user is not an admin
     if (adminOnly && userRole !== 'admin') {
       return <Unauthourized />;
     }
 
-    // Check if the route is moderator-only and the user is not a moderator
     if (moderatorOnly && userRole !== 'moderator') {
       return <Unauthourized />;
     }
+  }
 
-// Check if the user role is 'default' and show the Unauthourized component
   if (userRole === 'default') {
     return <Unauthourized />;
-  }
   }
 
   return children;
