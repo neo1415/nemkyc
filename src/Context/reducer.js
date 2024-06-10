67cdec51722bde3965253573b945850bb0777d5c
neo1415 <adedaniel502@gmail.com>
2024-06-10 23:49:44 +0100

    tried csrf again

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,36 +1,43 @@
 // reducer.js
 import {
-    SET_DATA,
-    SET_FILTERED_DATA,
-    SET_IS_LOADING,
-    SET_MODAL_OPEN,
-    SET_ID_TO_DELETE
-  } from './actionTypes';
-  
-  const initialState = {
-    data: [],
-    filteredData: [],
-    isLoading: false,
-    modalOpen: false,
-    idToDelete: null
-  };
-  
-  const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case SET_DATA:
-        return { ...state, data: action.data };
-      case SET_FILTERED_DATA:
-        return { ...state, filteredData: action.filteredData };
-      case SET_IS_LOADING:
-        return { ...state, isLoading: action.isLoading };
-      case SET_MODAL_OPEN:
-        return { ...state, modalOpen: action.modalOpen };
-      case SET_ID_TO_DELETE:
-        return { ...state, idToDelete: action.idToDelete };
-      default:
-        return state;
-    }
-  };
-  
-  export default rootReducer;
-  
+  SET_DATA,
+  SET_FILTERED_DATA,
+  SET_IS_LOADING,
+  SET_MODAL_OPEN,
+  SET_ID_TO_DELETE,
+  SET_EDIT_DATA,
+  SET_EDITING_KEY
+} from './actionTypes';
+
+const initialState = {
+  data: [],
+  filteredData: [],
+  isLoading: false,
+  modalOpen: false,
+  idToDelete: null,
+  editData: {}, // Add this
+  editingKey: null // Add this
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_DATA:
+      return { ...state, data: action.data };
+    case SET_FILTERED_DATA:
+      return { ...state, filteredData: action.filteredData };
+    case SET_IS_LOADING:
+      return { ...state, isLoading: action.isLoading };
+    case SET_MODAL_OPEN:
+      return { ...state, modalOpen: action.modalOpen };
+    case SET_ID_TO_DELETE:
+      return { ...state, idToDelete: action.idToDelete };
+    case SET_EDIT_DATA:
+      return { ...state, editData: action.data }; // Add this case
+    case SET_EDITING_KEY:
+      return { ...state, editingKey: action.key }; // Add this case
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
