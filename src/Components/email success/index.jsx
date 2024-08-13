fd6ad97a78f343638968bb9b4584b1cee1b68444
neo1415 <adedaniel502@gmail.com>
2024-08-13 03:59:15 +0100

    password completer

--- src/Components/email success/index.jsx
+++ src/Components/email success/index.jsx
@@ -3,12 +3,15 @@ import './SuccessEmail.css'; // Import the CSS file
 
 const SuccessEmail = () => {
   return (
-    <div className="email-card">
+    <div className='body'>
+
+<div className="email-card">
       <div className="checkmark-container">
         <i className="checkmark">✓</i>
       </div>
       <h1 className='success-h1'>Email Sent</h1>
       <p className='success-p'>You have been sent a password reset email;<br /> Please update your password to continue!</p>
+    </div>
     </div>
   );
 };
