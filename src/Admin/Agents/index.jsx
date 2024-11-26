0e84951bfb0e86eb35ae7411eb51c2b76b0592bd
neo1415 <adedaniel502@gmail.com>
2024-11-26 22:07:57 +0100

    roles and autho

--- src/Admin/Agents/index.jsx
+++ src/Admin/Agents/index.jsx
@@ -73,7 +73,7 @@ const AgentsList = () => {
       if (response.status === 200) {
         const data = response.data;
         // Filter out items with status 'processing' if user role is not 'admin'
-        const filtered = userRole === 'admin' ? data : data.filter(item => item.status !== 'processing');
+        const filtered = userRole === 'admin' || userRole === 'moderator'|| userRole === 'superAdmin'? data : data.filter(item => item.status !== 'processing');
         setData(filtered);
         setFilteredData(filtered);
       } else {
--- src/Admin/Partners/index.jsx
+++ src/Admin/Partners/index.jsx
@@ -63,7 +63,8 @@ const PartnersList = () => {
       if (response.status === 200) {
         const data = response.data;
         // Filter out items with status 'processing' if user role is not 'admin'
-        const filtered = userRole === 'admin' ? data : data.filter(item => item.status !== 'processing');
+        const filtered = userRole === 'admin' || userRole === 'moderator'|| userRole === 'superAdmin'? data : data.filter(item => item.status !== 'processing');
+        setData(filtered);
         setData(filtered);
         setFilteredData(filtered);
       } else {
