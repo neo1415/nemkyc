74da91e1dccfeccee28ebbb29625373be8a6ea4d
neo1415 <adedaniel502@gmail.com>
2025-03-13 08:23:40 +0100

    compliance endpoint

--- src/Admin/Authentication/Points.js
+++ src/Admin/Authentication/Points.js
@@ -15,7 +15,7 @@ export const endpoints = {
   getIndividualData: `${serverURL}/get-individual-data`,
   assignSuperAdminRole: (userId) => `${serverURL}/assign-super-admin-role/${userId}`,
   assignAdminRole: (userId) => `${serverURL}/assign-admin-role/${userId}`,
-  assignModeratorRole: (userId) => `${serverURL}/assign-moderator-role/${userId}`, // Add this line
+  assignComplianceRole: (userId) => `${serverURL}/assign-compliance-role/${userId}`, // Add this line
   assignDefaultRole: (userId) => `${serverURL}/assign-default-role/${userId}`, // Add this line
   checkAdminClaim: `${serverURL}/check-admin-claim`,
   checkUserRole: (userId) => `${serverURL}/check-user-role/${userId}`,
