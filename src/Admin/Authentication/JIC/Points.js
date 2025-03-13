a7c64ba323785171a6367a5e03f5f47d217d5800
neo1415 <adedaniel502@gmail.com>
2025-03-13 07:58:12 +0100

    moderator to compliance

--- src/Admin/Authentication/JIC/Points.js
+++ src/Admin/Authentication/JIC/Points.js
@@ -5,7 +5,7 @@ export   const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost
 export const endpoints = {
   getUsers: `${serverURL}/get-users`,
   assignAdminRole: (userId) => `${serverURL}/assign-admin-role/${userId}`,
-  assignModeratorRole: (userId) => `${serverURL}/assign-moderator-role/${userId}`, // Add this line
+  assignComplianceRole: (userId) => `${serverURL}/assign-compliance-role/${userId}`, // Add this line
   assignDefaultRole: (userId) => `${serverURL}/assign-default-role/${userId}`, // Add this line
   checkAdminClaim: `${serverURL}/check-admin-claim`,
   checkUserRole: (userId) => `${serverURL}/check-user-role/${userId}`,
